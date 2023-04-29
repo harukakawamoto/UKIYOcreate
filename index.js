7fc328b508942cdf55f72f9a897dbc108ba03523
harukakawamoto <kawakawa5213@gmail.com>
2023-04-30 00:19:18 +0900

    JSの冗長なコードを改善

--- index.js
+++ index.js
@@ -17,33 +17,22 @@ const { createClient } = microcms;
                 // 親要素の取得
                 let element = document.getElementsByClassName('contents')
                 console.log(contents)
-                for(let i = 0; i < contents.length;i = i+2){
+                for(let i = 0; i < contents.length;i++){
                     // div要素とimg要素とp要素の生成
-                    let parentDivElement = document.createElement('div')
                     let createDivElement1 = document.createElement('div')
-                    let createDivElement2 = document.createElement('div')
                     let imgElement1 = document.createElement('img')
-                    let imgElement2 = document.createElement('img')
                     let pElement1 = document.createElement('p')
-                    let pElement2 = document.createElement('p')
-                    
-                    parentDivElement.className = 'content'
+                    // classnameの指定
+                    createDivElement1.className = 'content'
+                    // 要素にAPIで取得した画像とテキストを挿入
                     imgElement1.src = contents[i].eyecatch.url
-                    imgElement2.src = contents[i+1].eyecatch.url
                     pElement1.textContent = contents[i].title
-                    pElement2.textContent = contents[i+1].title
-
+                    // divに画像とテキストの要素を追加
                     createDivElement1.appendChild(imgElement1)
                     createDivElement1.appendChild(pElement1)
-                    createDivElement2.appendChild(imgElement2)
-                    createDivElement2.appendChild(pElement2)
-
-                    parentDivElement.appendChild(createDivElement1)
-                    parentDivElement.appendChild(createDivElement2)
-
-                    element[0].appendChild(parentDivElement)
+                    // 親要素にcontentのDivを追加
+                    element[0].appendChild(createDivElement1)
                 }
             }
-            
             )
         .catch((err) => console.error(err));
